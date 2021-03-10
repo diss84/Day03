9a07ec41745bd6c946aa04524579b158ddc6527a
Mirela <adisserdarevic@gmail.com>
2021-03-10 20:35:09 +0100

    Added file Index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Izmjena single komentara
+    console.log ('Hello World!')
+    // Izmjena single komentara
+    /* document.write ("Ovo je novi komentar!");
+    document.write ("Ovo je stari komentar!");
+    */
+}
+console.log(main());
+
+module.exports = main;
